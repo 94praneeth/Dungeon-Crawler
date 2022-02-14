0b3bb3141cf12ed72f5ae0f719659ae2518d2e2c
94praneeth <94praneeth@gmail.com>
2022-02-14 23:54:23 +0530

    Halfway done. Added enemies to the map.

---
+++ src/components.rs
@@ -0,0 +1,13 @@
+use crate::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Copy)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Player;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Enemy;
