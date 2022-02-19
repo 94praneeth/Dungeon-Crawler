c99a5ecf844c5e275028a8742cfb37ed29100077
94praneeth <94praneeth@gmail.com>
2022-02-20 00:17:47 +0530

    Added random mobility to monsters

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -1,16 +1,12 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(
-    #[resource]map: &Map,
-    #[resource]camera: &Camera
-)
-{
+pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
-    for y in camera.top_y ..= camera.bottom_y {
-        for x in camera.left_x ..= camera.right_x {
-            let point = Point::new(x,y);
+    for y in camera.top_y..=camera.bottom_y {
+        for x in camera.left_x..=camera.right_x {
+            let point = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
             if map.in_bounds(point) {
                 let index = map_index(x, y);
@@ -18,7 +14,7 @@ pub fn map_render(
                     TileType::Floor => to_cp437('.'),
                     TileType::Wall => to_cp437('#'),
                 };
-                draw_batch.set(point - offset, ColorPair::new(WHITE,BLACK), glyph);
+                draw_batch.set(point - offset, ColorPair::new(WHITE, BLACK), glyph);
             }
         }
     }
